ab551501c867ceb8a71ffebbf7620e09f80016ef
Vikash <svikashkumar853@gmail.com>
2026-02-09 23:03:41 +0530

    testing

--- myapp/testing/math.go
+++ myapp/testing/math.go
@@ -3,3 +3,7 @@ package main
 func add(a, b int) int {
 	return a + b
 }
+
+func subtract(a, b int) int {
+	return a - b
+}
--- myapp/testing/math_test.go
+++ myapp/testing/math_test.go
@@ -4,7 +4,16 @@ import "testing"
 
 func TestAdd(t *testing.T) {
 	result := add(2, 3)
-	expected := 6
+	expected := 5
+
+	if result != expected {
+		t.Errorf("Expected %d but got %d", expected, result)
+	}
+}
+
+func TestSubtract(t *testing.T) {
+	result := subtract(5, 2)
+	expected := 3
 
 	if result != expected {
 		t.Errorf("Expected %d but got %d", expected, result)
